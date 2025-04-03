fbb8a0da01a94625cb72a84df2742acff884f18d
Owais Islam <owaisislam@gmail.com>
2025-04-02 20:31:36 -0500

    basic layout created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,130 +1,92 @@
-localStorage.clear();
+ localStorage.clear();
 
-function findCity() {
-    var cityName = titleCase($("#cityName")[0].value.trim());
+var apiURL = "https://api.postscript.io/api/v2/subscribers";
 
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer sk_6cd914fbb2c682a0626e85de06faed40'
+    }
+};
 
-    fetch(apiURL).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
+fetch(apiURL, options)
+    .then(res => res.json())
+    .then(function(res) {
+        var numberOfSubs = res.subscribers.length;
 
-                $("#city-name")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
+        const tableBody = document.querySelector('tbody');
 
-                $("#city-list").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
+        console.log("Num of Subscribers: "+ numberOfSubs);
 
-                const lat = data.coord.lat;
-                const lon = data.coord.lon;
+        for (var i = 0; i < numberOfSubs; i++) {
+            console.log("Phone Number " + (i + 1) + ": " + res.subscribers[i].phone_number);
+            console.log("Email " + (i + 1) + ": " + res.subscribers[i].email);
+            console.log("Created Date " + (i + 1) + ": " + res.subscribers[i].created_at);
+            console.log("Tags " + (i + 1) + ": " + res.subscribers[i].tags);
 
-                var latLonPair = lat.toString() + " " + lon.toString();
-
-                localStorage.setItem(cityName, latLonPair);
-
-                apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
-
-                fetch(apiURL).then(function (newResponse) {
-                    if (newResponse.ok) {
-                        newResponse.json().then(function (newData) {
-                            getCurrentWeather(newData);
-                        })
-                    }
-                })
-            })
-        } else {
-            alert("Cannot find city!");
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <th scope="row">${res.subscribers[i].phone_number}</th>
+                <td>${res.subscribers[i].email}</td>
+                <td>${moment(res.subscribers[i].created_at).format('MM/DD/YYYY')}</td>
+                <td>${res.subscribers[i].tags.join(', ')}</td>
+                <td><button class="btn btn-primary">Add Tag</button></td>
+            `;
+            tableBody.appendChild(row);
         }
     })
-}
-
-// This function gets the info for a city already in the list. It does not need to check whether the city exists as it was already checked when the city was first searched for.
-function getListCity(coordinates) {
-    apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
-
-    fetch(apiURL).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                getCurrentWeather(data);
-            })
-        }
-    })
-}
-
-function getCurrentWeather(data) {
-    $(".results-panel").addClass("visible");
-
-    $("#currentIcon")[0].src = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png";
-    $("#temperature")[0].textContent = "Temperature: " + data.current.temp.toFixed(1) + " \u2109";
-    $("#humidity")[0].textContent = "Humidity: " + data.current.humidity + "% ";
-    $("#wind-speed")[0].textContent = "Wind Speed: " + data.current.wind_speed.toFixed(1) + " MPH";
-    $("#uv-index")[0].textContent = "  " + data.current.uvi;
-
-    if (data.current.uvi < 3) {
-        $("#uv-index").removeClass("moderate severe");
-        $("#uv-index").addClass("favorable");
-    } else if (data.current.uvi < 6) {
-        $("#uv-index").removeClass("favorable severe");
-        $("#uv-index").addClass("moderate");
-    } else {
-        $("#uv-index").removeClass("favorable moderate");
-        $("#uv-index").addClass("severe");
+    .catch(err => console.error(err)); {
+        console.log("Error fetching data");
     }
 
-    getFutureWeather(data);
-}
-
-function getFutureWeather(data) {
-    for (var i = 0; i < 5; i++) {
-        var futureWeather = {
-            date: convertUnixTime(data, i),
-            icon: "http://openweathermap.org/img/wn/" + data.daily[i + 1].weather[0].icon + "@2x.png",
-            temp: data.daily[i + 1].temp.day.toFixed(1),
-            humidity: data.daily[i + 1].humidity
-        }
-
-        var currentSelector = "#day-" + i;
-        $(currentSelector)[0].textContent = futureWeather.date;
-        currentSelector = "#img-" + i;
-        $(currentSelector)[0].src = futureWeather.icon;
-        currentSelector = "#temp-" + i;
-        $(currentSelector)[0].textContent = "Temp: " + futureWeather.temp + " \u2109";
-        currentSelector = "#hum-" + i;
-        $(currentSelector)[0].textContent = "Humidity: " + futureWeather.humidity + "%";
-    }
-}
-
-// This function applies title case to a city name if there is more than one word.
-function titleCase(city) {
-    var updatedCity = city.toLowerCase().split(" ");
-    var returnedCity = "";
-    for (var i = 0; i < updatedCity.length; i++) {
-        updatedCity[i] = updatedCity[i][0].toUpperCase() + updatedCity[i].slice(1);
-        returnedCity += " " + updatedCity[i];
-    }
-    return returnedCity;
-}
-
-// This converts the UNIX time that is received from the server.
-function convertUnixTime(data, index) {
-    const dateObject = new Date(data.daily[index + 1].dt * 1000);
-
-    return (dateObject.toLocaleDateString());
-}
-
-$("#search-button").on("click", function (e) {
-    e.preventDefault();
-
-    findCity();
-
-    $("form")[0].reset();
-})
-
-$(".city-list-box").on("click", ".city-name", function () {
-
-    var coordinates = (localStorage.getItem($(this)[0].textContent)).split(" ");
-    coordinates[0] = parseFloat(coordinates[0]);
-    coordinates[1] = parseFloat(coordinates[1]);
-
-    $("#city-name")[0].textContent = $(this)[0].textContent + " (" + moment().format('M/D/YYYY') + ")";
-
-    getListCity(coordinates);
-})
+    document.addEventListener('DOMContentLoaded', function () {
+        const content = document.querySelector('.content'); 
+        const itemsPerPage = 5;
+        let currentPage = 0;
+        const items = Array.from(content.getElementsByTagName('tr')).slice(1);
+      
+      function showPage(page) {
+        const startIndex = page * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+        items.forEach((item, index) => {
+          item.classList.toggle('hidden', index < startIndex || index >= endIndex);
+        });
+        updateActiveButtonStates();
+      }
+      
+      function createPageButtons() {
+        const totalPages = Math.ceil(items.length / itemsPerPage);
+        const paginationContainer = document.createElement('div');
+        const paginationDiv = document.body.appendChild(paginationContainer);
+        paginationContainer.classList.add('pagination');
+      
+        // Add page buttons
+        for (let i = 0; i < totalPages; i++) {
+          const pageButton = document.createElement('button');
+          pageButton.textContent = i + 1;
+          pageButton.addEventListener('click', () => {
+            currentPage = i;
+            showPage(currentPage);
+            updateActiveButtonStates();
+          });
+      
+            content.appendChild(paginationContainer);
+            paginationDiv.appendChild(pageButton);
+          }
+      }
+      
+      function updateActiveButtonStates() {
+        const pageButtons = document.querySelectorAll('.pagination button');
+        pageButtons.forEach((button, index) => {
+          if (index === currentPage) {
+            button.classList.add('active');
+          } else {
+            button.classList.remove('active');
+          }
+        });
+      }
+      
+        createPageButtons(); // Call this function to create the page buttons initially
+        showPage(currentPage);
+      });
